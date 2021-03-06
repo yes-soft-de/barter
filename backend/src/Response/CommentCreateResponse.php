ff5ac14192de98c15a9404e1d08baf67b1a63194
ralsalloum <ramisalloum12@hotmail.com>
2021-03-06 21:37:21 +0200

    Comment entity was being added

---
+++ backend/src/Response/CommentCreateResponse.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Response;
+
+
+class CommentCreateResponse
+{
+    public $id;
+    
+    public $createdBy;
+    
+    public $createdAt;
+
+    public $entityID;
+
+    public $text;
+}
